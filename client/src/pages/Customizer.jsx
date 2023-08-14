52341385302b53912179c0621fef6cb027e9fa1c
Johnson Kow <johnson@somble.com>
2023-08-13 19:52:57 -0700

    updated fetch backend url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -43,7 +43,7 @@ const Customizer = () => {
       // Call backend to generate an AI Image 
       setGeneratingImg(true);
 
-    const response = await fetch('https://devswag.onrender.com/api/v1/dalle', {
+    const response = await fetch('https://project-3js-ai-1xsf.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
